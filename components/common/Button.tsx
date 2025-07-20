90187c71b727f011948a8e514821386b3a548b06
Huomagift <huomagift@gmail.com>
2025-07-20 22:55:15 +0100

    Created responsive listing page

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,8 +1,12 @@
-const Button = () => {
-    return(
-        <p> Button </p>
+import { ButtonProps } from "@/interfaces";
+import React from "react";
+
+const Button: React.FC<ButtonProps> = ({label, styles}) => {
+    return (
+        <button className={`px-4 py-2 font-medium ${styles}`}>
+            {label}
+        </button>
     )
 }
-
 
 export default Button;
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,16 +1,33 @@
 // Props for Card component
 export interface CardProps {
-  title: string
-  description?: string
-  imageUrl?: string
-  onClick?: () => void
+  property: PropertyProps;
 }
 
 // Props for Button component
 export interface ButtonProps {
   label: string
+  styles?: string
   onClick?: () => void
   type?: 'button' | 'submit' | 'reset'
   disabled?: boolean
   variant?: 'primary' | 'secondary' | 'danger'
+}
+
+export interface PropertyProps {
+  name: string,
+    address: {
+      state: string,
+      city: string,
+      country: string
+    },
+    rating: number,
+    category: [string, string, string],
+    price: number,
+    offers: {
+      bed: string,
+      shower: string,
+      occupants: string
+    },
+    image: string,
+    discount: string
 }
